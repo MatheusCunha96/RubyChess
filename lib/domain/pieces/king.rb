c14869561c9440d219947fe3b91310038f640ac9
Matheus Cunha <matheuscunha96@hotmail.com>
2022-07-11 12:15:15 -0300

    add initial logic to king movement

--- lib/domain/pieces/king.rb
+++ lib/domain/pieces/king.rb
@@ -4,6 +4,43 @@ require_relative 'piece'
 
 class King < Piece
   def initialize(color)
+    @move_set = set_move_set
     super(color)
+  end
+
+  def find_moves(board)
+    moves = []
+
+    @move_set.each do |direction|
+      column = current_col + direction[0]
+      row = current_row + direction[1]
+
+      next if Piece.out_of_bounds?(row, column)
+
+      position_state = board.position_state(row, column)
+
+      if position_state.nil?
+        moves << [row, column]
+      elsif position_state != @color
+        moves << [row, column]
+      end
+    end
+
+    moves
+  end
+
+  private
+
+  def set_move_set
+    [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1]
+    ]
   end
 end
---
+++ spec/domain/pieces/king_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'domain/pieces/king'
+# require 'domain/position'
+require 'domain/board'
+
+describe King do
+  let(:board) { Board.new }
+
+  after { reset_board(board) }
+
+  describe '.new' do
+    let(:color) { 'white' }
+    let(:king) { King.new(color) }
+
+    it 'current position must initialize nil' do
+      expect(king.current_position).to be_nil
+    end
+
+    it 'must have color' do
+      expect(king.color).to eql(color)
+    end
+
+    describe 'move_set' do
+      it 'must have correct possibles move' do
+        king = Queen.new('white')
+        expect(king.move_set).to eql(
+          [[0, 1],
+           [0, -1],
+           [1, 0],
+           [-1, 0],
+           [1, 1],
+           [1, -1],
+           [-1, 1],
+           [-1, -1]]
+        )
+      end
+    end
+  end
+
+end
